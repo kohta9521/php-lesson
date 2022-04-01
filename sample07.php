121cc75baa85a13404e3a2b4d7318f49ec1b1f93
k_kohta95 <100065508+kohta9521@users.noreply.github.com>
2022-04-01 21:11:12 +0900

    Add files via upload

---
+++ sample07.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+
+       $i = 1;
+       echo $i . '<br>';
+
+       $i = $i + 1;
+       echo $i . '<br>';
+
+
+
+       while ($i < 366) {
+        $i = $i + 1;
+        echo $i . '<br>';
+           
+       }
+       
+    ?>
+    
+</body>
+</html>
